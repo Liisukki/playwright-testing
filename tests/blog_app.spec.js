6792c39f5802d10532c9ce8b8ff3673e52b44620
LiisaKotilainen <liisa.kotilainen@edu.turkuamk.fi>
2025-01-27 18:22:57 +0200

    5.18 blogilistan end to end ‑testit, step2

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -5,10 +5,10 @@ describe("Blog app", () => {
     // Nollaa tietokanta
     await request.post("http://localhost:3003/api/testing/reset");
 
-    // Luo uusi käyttäjä
+    // Uusi käyttäjä
     await request.post("http://localhost:3003/api/users", {
       data: {
-        name: "Liisukki Testaaja",
+        name: "Liisu Kotilainen",
         username: "liisukki",
         password: "salainen",
       },
@@ -20,12 +20,44 @@ describe("Blog app", () => {
 
   test("Login form is shown", async ({ page }) => {
     // Tarkista, että kirjautumislomakkeen elementit näkyvät
-    const usernameInput = await page.getByLabel("username");
-    const passwordInput = await page.getByLabel("password");
+    const usernameInput = await page.getByLabel("Username:");
+    const passwordInput = await page.getByLabel("Password:");
     const loginButton = await page.getByRole("button", { name: /login/i });
 
     await expect(usernameInput).toBeVisible();
     await expect(passwordInput).toBeVisible();
     await expect(loginButton).toBeVisible();
+  });
+
+  describe("Login", () => {
+    // Oikeat kirjautumistiedot
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByLabel("Username:").fill("liisukki");
+      await page.getByLabel("Password:").fill("salainen");
+
+      // Koita kirjautua
+      await page.getByRole("button", { name: /login/i }).click();
+
+      // Tarkista, että kirjautuminen onnistui ja käyttäjän nimi näkyy
+      await expect(page.getByText("Liisa Kotilainen logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      // Väärät kirjautumistiedot
+      await page.getByLabel("Username:").fill("liisukki");
+      await page.getByLabel("Password:").fill("väärä");
+
+      // Koita kirjautua
+      await page.getByRole("button", { name: /login/i }).click();
+
+      // Tarkista, että ilmoitus epäonnistuneesta kirjautumisesta näkyy
+      const errorNotification = await page.getByText(
+        "Wrong username or password"
+      );
+      await expect(errorNotification).toBeVisible();
+
+      // Varmista, että käyttäjä ei ole kirjautunut sisään
+      await expect(page.getByText("Liisa Kotilainen logged in")).toHaveCount(0);
+    });
   });
 });
